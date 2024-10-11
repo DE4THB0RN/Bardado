76817bdf2e033d79f40b9391f1af1a47f9e28de5
Pedro Augusto <127454807+DE4THB0RN@users.noreply.github.com>
2024-10-11 18:01:27 -0300

    Bora la

--- src/secret.rs
+++ src/secret.rs
@@ -1,5 +1,5 @@
 
-const DISCORD_TOKEN: String = "MTI2NjEzMTczNjQ0NjYzMTk1Ng.Gy2m02.xp6Al4kBwqs_jI2urPOcrIZE0dzubyxyVbYKY8".to_string();
+const DISCORD_TOKEN: String = "baaa".to_string();
 
 pub fn get_discord_token() -> String {
     DISCORD_TOKEN
